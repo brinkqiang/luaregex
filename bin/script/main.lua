47761f5236abc57d8e0188bf275fda2966c36bce
brinkqiang <brink.qiang@gmail.com>
2021-12-01 17:40:37 +0800

    add

--- bin/script/main.lua
+++ bin/script/main.lua
@@ -4,8 +4,14 @@ local luaregex = require("luaregex")
 
 local reg = luaregex.luaregex.new()
 
-local ret = reg:search("hello htylo htyto htylt", "(h\\w+lo)")
+local ret = reg:match("hello htylo htyto htylt", "(h\\w+lo)")
 
 for i, v in ipairs(ret) do
+    print(i, v)
+end
+
+local ret2 = reg:search("hello htylo htyto htylt", "(h\\w+lo)")
+
+for i, v in ipairs(ret2) do
     print(i, v)
 end
